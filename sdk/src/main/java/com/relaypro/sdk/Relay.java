188b9b2d533b69a36a44088380da1dc600aa99fc
Ashley Godwin <agodwin@relaypro.com>
2022-07-15 16:42:50 -0400

    Made TargetUri into type class

--- sdk/src/main/java/com/relaypro/sdk/Relay.java
+++ sdk/src/main/java/com/relaypro/sdk/Relay.java
@@ -584,12 +584,12 @@ public class Relay {
     }
 
     // Creates a target URI object that is needed when sending out notifications to groups
-    private class TargetUri {
-        private String[] uris;
-        public TargetUri(String[] uris) {
-            this.uris = uris;
-        }
-    }
+    // private class TargetUri {
+    //     private String[] uris;
+    //     public TargetUri(String[] uris) {
+    //         this.uris = uris;
+    //     }
+    // }
 
     private void sendNotification(String target, String originator, String type, String text, String name) {
         logger.debug("Sending notification with name: " + name);
