05e41843f5a116de9a01a7b968e0798eda4803ad
Ashley Godwin <agodwin@relaypro.com>
2022-09-09 11:40:08 -0400

    Refined package documentation

--- sdk/src/main/java/com/relaypro/sdk/package-info.java
+++ sdk/src/main/java/com/relaypro/sdk/package-info.java
@@ -1,8 +1,10 @@
 // Copyright © 2022 Relay Inc.
 
 /**
- * These are the classes that provides the main implementation of the SDK.  It includes the Relay API, where the main
- * method calls the bridge communication between the client and server live, a utility class for building requests and
- * performing other utilities needed by the SDK, URI parse/create methods, and classes for websocket communication.
+ * These are the classes that provide the main implementation of the SDK.  This includes the Relay API, which contains
+ * the main method calls that bridge communication between the client and server.  It also includes
+ * a utility class for building requests, creating a target object and performing other utilities needed by the SDK, as
+ * well as a URI parse/create class, a JakartaWebsocketReceiver class, objects for events and requests, and different
+ * workflow methods.
  */
 package com.relaypro.sdk;
--- sdk/src/main/java/com/relaypro/sdk/types/package-info.java
+++ sdk/src/main/java/com/relaypro/sdk/types/package-info.java
@@ -3,7 +3,7 @@
 /**
  * These are some Java object types used in the SDK methods.  Each of the objects
  * within this package include serialized fields that are specific to that object and are used to communicate
- * with the server. The field values are filled in within the API, which is when an API call utilizes one of the events or
- * responses, or by the user who passes in values to those API calls that are sent in a message to the sever.
+ * with the server. The field values are filled in when an API call utilizes one of the events or
+ * responses, or by the user who passes in values to those API methods, which are then sent in a message to the sever.
  */
 package com.relaypro.sdk.types;
