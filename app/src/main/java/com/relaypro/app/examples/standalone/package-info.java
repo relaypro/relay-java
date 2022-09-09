cc34a90a67e25883aa90a602942a014a71e6f51a
Ashley Godwin <agodwin@relaypro.com>
2022-09-09 11:53:24 -0400

    Package wasn't fully addd, adding here

--- app/src/main/java/com/relaypro/app/examples/standalone/package-info.java
+++ app/src/main/java/com/relaypro/app/examples/standalone/package-info.java
@@ -1,1 +1,7 @@
+// Copyright © 2022 Relay Inc.
+
+/**
+ * This is a very simple, single, standalone workflow. The workflow is added and the server is started within the
+ * workflow class, instead of having a separate Setup class for adding multiple different workflows.
+ */
 package com.relaypro.app.examples.standalone;
